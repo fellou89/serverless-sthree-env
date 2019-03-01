3a74e384e5ea260fdd0b7604966274d7ffea1a59
fellou89 <auribe@Alferdos-MacBook-Pro.local>
2019-03-01 09:41:45 -0500

    my version

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
 module.exports = class SthreeEnvPlugin {
-    constructor(serverless, options){
-        this._serverless = serverless;
-        this._options = options;
-        this.hooks = {
-            'before:deploy:createDeploymentArtifacts':this.beforeDeployResource.bind(this)
+  constructor(serverless, options) {
+    this._serverless = serverless;
+    this._options = options;
+    this.hooks = {
+      'before:package:createDeploymentArtifacts': this.beforeDeployResource.bind(this),
+    };
+  }
+
+  beforeDeployResource() {
+    this._serverless.cli.log('SthreeEnvPlugin : ' + 'is running');
+    let stage = this._options.stage ? this._options.stage : this._serverless.service.provider.stage;
+    let region = this._serverless.service.provider.region;
+
+    let resourceName = this._serverless.service.custom.Loglist.Bucket;
+    let keyName = this._serverless.service.custom.Loglist.Key;
+
+    return this.getSThreeBucket(resourceName, keyName, stage, region).then(
+      function(data, err) {
+        this._serverless.cli.log('SthreeEnvPlugin : getting config from bucket ' + resourceName);
+        if (err) {
+          this._serverless.cli.log('SthreeEnvPlugin : ' + err);
         }
-    }
+        let body = data.Body;
 
-    getSThreeBucket(bucketName, keyName, stage, region) {
-        return this._serverless.getProvider(this._serverless.service.provider.name).request('S3', 'getObject', {
-            Bucket: bucketName,
-            Key: keyName,
-        }, stage, region);
-    }
+        this._serverless.service.functions.logs.events = JSON.parse(body);
 
-    appendEnvironment(currData, data) {
-        let obj = {};
-        obj = JSON.parse(data);
-        //do other stuff later on
-        return Object.assign(obj, currData);
-    }
+        return true
+      }.bind(this),
+    );
+  }
 
-    beforeDeployResource() {
-        this._serverless.cli.log('SthreeEnvPlugin : ' + 'is running');
-        let stage = (this._options.stage ? this._options.stage : this._serverless.service.provider.stage);
-        let region = this._serverless.service.provider.region;
-        let resourceName = this._serverless.service.service + '-config-' + stage;
-
-        return this.getSThreeBucket(resourceName, 'config.json', stage, region).then(function(data, err){
-            this._serverless.cli.log('SthreeEnvPlugin : getting config from bucket '+ resourceName);
-            if (err) {
-                this._serverless.cli.log('SthreeEnvPlugin : ' + err);
-            }
-            let body = data.Body;
-            try {
-                this._serverless.service.provider.environment = this.appendEnvironment(this._serverless.service.provider.environment, body);
-                this._serverless.cli.log('SthreeEnvPlugin : env var contains '+ Object.keys(this._serverless.service.provider.environment));
-            } catch(e) {
-                this._serverless.cli.log('SthreeEnvPlugin : ' + e);
-            }
-            return true;
-        }.bind(this));
-    }
+  getSThreeBucket(bucketName, keyName, stage, region) {
+    return this._serverless.getProvider(this._serverless.service.provider.name).request(
+      'S3',
+      'getObject',
+      {
+        Bucket: bucketName,
+        Key: keyName,
+      },
+      stage,
+      region,
+    );
+  }
 };
